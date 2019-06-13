2d97f94430930538b386bb1f4ea32168a55e4c48
SAIPHER ZURETI <saipherzureti@gmail.com>
2019-06-13 14:35:30 -0500

    Updated this principles, still working

--- assignments/this.js
+++ assignments/this.js
@@ -1,9 +1,9 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. Window/Global Object Binding
-* 2. Implicit Binding
-* 3. New binding
+* 1. Window/Global Object Binding - "this" is automatically set to the window or console.
+* 2. Implicit Binding - "this" is provided implicitly by a preceding ".", then followed by a predefined method.
+* 3. New binding - "this" 
 * 4. Explicit binding
 *
 * write out a code example of each explanation above
@@ -11,11 +11,20 @@
 
 // Principle 1
 
-// code example for Window Binding
+function myName(name){
+    console.log(this);
+    return name;
+}
+myName("Saipher");
 
 // Principle 2
 
-// code example for Implicit Binding
+const zureti = {
+    name: "Zureti"
+    say: function(){ console.log(`My last name is ${this.name}.`)}
+}
+
+zureti.say();
 
 // Principle 3
 
